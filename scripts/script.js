652b336f6e11aa3b069a21e81394c237bfa02655
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2024-02-14 17:03:46 -0300

    implementações finais do projeto

--- scripts/script.js
+++ scripts/script.js
@@ -75,6 +75,11 @@ const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
     musicaBeep.play();
     alert("Tempo finalizado!");
+    const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+    if(focoAtivo){
+      const evento = new CustomEvent('FocoFinalizado');
+      document.dispatchEvent(evento);
+    }
     zerar();
     return;
   }
